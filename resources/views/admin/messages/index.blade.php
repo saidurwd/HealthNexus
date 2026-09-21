9d31ec533b574fd167ace74dcdb9e8c62c8de875
Saidur Rahman <saidurwd@gmail.com>
2026-09-21 15:57:02 +0600

    Create index.blade.php

---
+++ resources/views/admin/messages/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.adminlte')
+
+@section('page_title', 'Messages')
+
+@section('page_content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Messages</h3>
+        </div>
+        <div class="card-body">
+            <div class="alert alert-info">
+                No messages available.
+            </div>
+        </div>
+    </div>
+@stop
